8bc5ed402ea06d0de9088751d00b141438c295aa
Danilo Morales <darmito10@gmail.com>
2025-10-23 13:46:13 -0500

    Refactor domain environment variable handling to remove default value

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -27,7 +27,7 @@ load_dotenv()
 SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 ALGORITHM = os.getenv("JWT_ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("JWT_EXPIRE_MINUTES", 30))
-DOMAIN = os.getenv("DOMAIN", "localhost")
+DOMAIN = os.getenv("DOMAIN")
 REFRESH_TOKEN_EXPIRE_DAYS = int(os.getenv("JWT_REFRESH_EXPIRE_DAYS", 7))
 SECURE_COOKIE = not is_dev()
 COOKIE_SAMESITE =  "lax" if is_dev() else "none"
